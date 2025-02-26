35ea65cb347d29b2e534fd66a1a2e377a35b64a0
trantu1243 <trantu1242003@gmail.com>
2025-02-27 03:16:45 +0700

    fixed

--- server/dump/bill.js
+++ server/dump/bill.js
@@ -23,24 +23,26 @@ async function billToMongo() {
             if (!existingAccount) {
                 const staff = await Staff.findOne({ email: item.created_by });
                 const box = await BoxTransaction.findOne({ initialId: Number(item.box_id) });
-
-                const newItem = await Bill.create({
-                    initialId: item.id,
-                    bankCode: item.bank_code,
-                    stk: item.account_id,
-                    content: item.content,
-                    amount: Number(item.amount),
-                    bonus: Number(item.bonus),
-                    typeTransfer: item.type_transfer,
-                    boxId: box._id,
-                    messengerId: item.messenger_id,
-                    linkQr: item.link_qr ? item.link_qr : '',
-                    status: Number(item.status),
-                    staffId: staff._id,
-                    isCompleted: item.is_completed,
-                    createdAt: createdAt.toISOString(),
-                    updatedAt: updatedAt.toISOString(),
-                });
+                if (staff) {
+                    const newItem = await Bill.create({
+                        initialId: item.id,
+                        bankCode: item.bank_code,
+                        stk: item.account_id,
+                        content: item.content,
+                        amount: Number(item.amount),
+                        bonus: Number(item.bonus),
+                        typeTransfer: item.type_transfer,
+                        boxId: box._id,
+                        messengerId: item.messenger_id,
+                        linkQr: item.link_qr ? item.link_qr : '',
+                        status: Number(item.status),
+                        staffId: staff?._id,
+                        isCompleted: item.is_completed,
+                        createdAt: createdAt.toISOString(),
+                        updatedAt: updatedAt.toISOString(),
+                    });
+                }
+               
             }
         }
         console.log('Hoàn tất cập nhật Bill dữ liệu vào MongoDB');
