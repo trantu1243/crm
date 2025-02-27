8651784ab3f655e8bd8bbe15f3025c4553af8450
trantu1243 <trantu1242003@gmail.com>
2025-02-27 11:49:02 +0700

    test

--- server/dump/index.js
+++ server/dump/index.js
@@ -14,13 +14,13 @@ const { billToMongo } = require('./bill');
 const { transactionToMongo } = require('./transaction');
 
 async function importExcelToMongo() {
-    bankAccountToMongo();
-    bankApiToMongo();
-    staffToMongo();
-     boxTransactionToMongo();
-     feeTransactionToMongo();
-     billToMongo();
-     transactionToMongo();
+    await bankAccountToMongo();
+    await bankApiToMongo();
+    await staffToMongo();
+    await boxTransactionToMongo();
+    await feeTransactionToMongo();
+    await billToMongo();
+    await transactionToMongo();
 }
 
 module.exports = {
