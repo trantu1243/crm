621e64e3561ee6f2cd5a922cc5fbacf3516fd41e
trantu1243 <trantu1242003@gmail.com>
2025-02-28 03:24:42 +0700

    dump

--- server/dump/index.js
+++ server/dump/index.js
@@ -1,6 +1,6 @@
 const xlsx = require('xlsx');
 const path = require('path');
-const { BankAccount } = require('../models');
+const { BankAccount, BoxTransaction, Transaction, Bill } = require('../models');
 const { bankAccountToMongo } = require('./bankAccount');
 const { staffToMongo } = require('./staff');
 const { bankApiToMongo } = require('./bankApi');
@@ -13,14 +13,38 @@ const { feeTransactionToMongo } = require('./feeTransaction');
 const { billToMongo } = require('./bill');
 const { transactionToMongo } = require('./transaction');
 
+
+async function deleteDocumentsWithoutInitialId() {
+    try {
+        const boxResult = await BoxTransaction.deleteMany({
+            $or: [{ initialId: { $exists: false } }, { initialId: null }]
+        });
+        console.log(`Deleted ${boxResult.deletedCount} BoxTransaction documents.`);
+        const transactionResult = await Transaction.deleteMany({
+            $or: [{ initialId: { $exists: false } }, { initialId: null }]
+        });
+        console.log(`Deleted ${transactionResult.deletedCount} Transaction documents.`);
+        const billResult = await Bill.deleteMany({
+            $or: [{ initialId: { $exists: false } }, { initialId: null }]
+        });
+
+        
+        
+        console.log(`Deleted ${billResult.deletedCount} Bill documents.`);
+    } catch (error) {
+        console.error("Error deleting documents:", error);
+    }
+}
+
 async function importExcelToMongo() {
+    await deleteDocumentsWithoutInitialId()
     // await bankAccountToMongo();
     // await bankApiToMongo();
     // await staffToMongo();
     // await boxTransactionToMongo();
     // await feeTransactionToMongo();
     // await billToMongo();
-    await transactionToMongo();
+    // await transactionToMongo();
 }
 
 module.exports = {
