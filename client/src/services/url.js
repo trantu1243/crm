7d88afc9e5321bfea1127e8068c50e8255c98b7b
Tran Tu <trantu1242003@gmail.com>
2025-03-01 15:45:51 +0700

    setup dev

--- client/src/services/url.js
+++ client/src/services/url.js
@@ -1,2 +1,3 @@
-export const SERVER_URL = 'https://mayman.tathanhan.com'
+// export const SERVER_URL = 'https://mayman.tathanhan.com'
+export const SERVER_URL = 'https://apidev.tathanhan.com'
 // export const SERVER_URL = 'http://localhost:3000'
