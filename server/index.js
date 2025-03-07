f5a98f74d304fe51ff891915664f3fe9017fb2ed
Tran Tu <trantu1242003@gmail.com>
2025-03-08 01:35:30 +0700

    .

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ mongoose.connect(process.env.MONGODB_URL).then(() => {
     // updateFlags()
     // updateSetting()
     // updateSetting()
-
+    updateSetting()
 });
 
 const updateSetting = async () =>{
