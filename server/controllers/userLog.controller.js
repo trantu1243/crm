3b09283fa2a0514d80f3c62c85abfe9df1fa30a3
trantu1243 <trantu1242003@gmail.com>
2025-03-13 17:09:30 +0700

    log

--- server/controllers/userLog.controller.js
+++ server/controllers/userLog.controller.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const { UserLog } = require("../models");
 
 const getLogs = async (req, res) => {
@@ -16,7 +17,8 @@ const getLogs = async (req, res) => {
 
 const getBillLogs = async (req, res) => {
     try {
-         const logs = await UserLog.find({action: 'CREATE_BILL'}).sort({createdAt: -1}).limit(3000);
+        const targetId = new mongoose.Types.ObjectId('67cab4e51e53f873cca99e69');
+        const logs = await UserLog.find({targetId: targetId}).sort({createdAt: -1}).limit(3000);
 
         res.status(200).json({
             message: 'Logs fetched successfully',
