c8865e2143a96f7c631e05f10e429b57fa4c41d0
trantu1243 <trantu1242003@gmail.com>
2025-04-13 03:24:10 +0700

    customer

--- client/src/services/url.js
+++ client/src/services/url.js
@@ -1,3 +1,3 @@
-// export const SERVER_URL = 'https://mayman.tathanhan.vn'
+export const SERVER_URL = 'https://mayman.tathanhan.vn'
 // export const SERVER_URL = 'https://apidev.tathanhan.com'
-export const SERVER_URL = 'http://localhost:3000'
+// export const SERVER_URL = 'http://localhost:3000'
